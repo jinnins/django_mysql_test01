e3896670f00ac6ce33fef94aefd85acf722e2bb4
jimin <86237475+jinnins@users.noreply.github.com>
2023-01-24 22:13:25 +0900

    second

--- test01/urls.py
+++ test01/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('test01datas/', views.getTestDatas, name="test01datas"),
     path('test01data/<str:name>', views.getTestData, name="test01data"),
+    path('postMember/', views.postMember, name="postMember"),
+    path('putMember/<str:pk>', views.putMember, name="putMember"),
 ]
--- test01/views.py
+++ test01/views.py
@@ -2,7 +2,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Test01
 from .serializers import TestDataSerializer
-
+from rest_framework import status
 
 @api_view(['GET'])
 def getTestDatas(request):
@@ -15,3 +15,22 @@ def getTestData(request, name):
     data = Test01.objects.get(name=name)
     serializer = TestDataSerializer(data, many=False)
     return Response(serializer.data)
+
+@api_view(['POST'])
+def postMember(request):
+    reqData = request.data
+    serializer = TestDataSerializer(data=reqData)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+def putMember(request, pk):
+    reqData = request.data
+    data = Test01.objects.get(id=pk)
+    serializer = TestDataSerializer(instance=data, data=reqData)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
